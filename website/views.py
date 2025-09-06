9ed9ca62087d7c909f63371b309a4230cff81bb2
yoones baghaei <96141027+yoonesbaghaei@users.noreply.github.com>
2025-09-06 13:39:37 +0330

    start blog app
    set urls blog on mysite app

--- website/views.py
+++ website/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request,"index.html")
+    return render(request,"site/index.html")
 
 def about_view(request):
-    return render(request,"about.html")
+    return render(request,"site/about.html")
 
 def contact_view(request):
-    return render(request,'contact.html')
+    return render(request,'site/contact.html')
+
